ca8151ecc0b44bed84213c46f2f7de0a032d5fb5
adamclear <3589@holbertonschool.com>
2022-05-13 00:40:47 +0000

    added files for tasks 9-10

---
+++ 0x04-TypeScript/task_4/js/subjects/Subjects.js
@@ -0,0 +1,17 @@
+/// <reference path="Teacher.ts" />
+var Subjects;
+(function (Subjects) {
+    var Subject = /** @class */ (function () {
+        function Subject() {
+        }
+        Object.defineProperty(Subject.prototype, "setTeacher", {
+            set: function (teach) {
+                this.teacher = teach;
+            },
+            enumerable: false,
+            configurable: true
+        });
+        return Subject;
+    }());
+    Subjects.Subject = Subject;
+})(Subjects || (Subjects = {}));
