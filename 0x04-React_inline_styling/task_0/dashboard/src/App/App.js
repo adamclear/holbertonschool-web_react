b79bf3c2ecee859bbd9433370f5c136497df04a5
adamclear <3589@holbertonschool.com>
2023-01-18 06:23:40 +0000

    added directory and updated for task 1

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/App/App.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './App.css';
+import Notifications from '../Notifications/Notifications';
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+import Login from '../Login/Login';
+import CourseList from '../CourseList/CourseList';
+import BodySection from '../BodySection/BodySection';
+import BodySectionWithMarginBottom from '../BodySection/BodySectionWithMarginBottom';
+import { getLatestNotification } from '../utils/utils';
+import WithLogging from '../HOC/WithLogging';
+
+const listCourses = [
+  { id: '1', name: 'ES6', credit: 60},
+  { id: '2', name: 'Webpack', credit: 20},
+  { id: '3', name: 'React', credit: 40},
+]
+
+const listNotifications = [
+  { id: 1, type: 'default', value: 'New course available' },
+  { id: 2, type: 'urgent', value: 'New resume available' },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+]
+
+export default class App extends React.Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.ctrlKeyPress);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.ctrlKeyPress);
+  }
+  
+  ctrlKeyPress = (event) => {
+    if (event.ctrlKey && event.key === 'h') {
+        window.alert('Logging you out')
+        this.props.logOut();
+      }
+  }
+
+  render () {
+    const {isLoggedIn} = this.props;
+    return (
+      <React.Fragment>
+        <div className="App">
+          <Notifications listNotifications={listNotifications} />
+          <div className="App-header">
+            <Header />
+          </div>
+          <div className="App-body">
+            {isLoggedIn ? (
+              <BodySectionWithMarginBottom title="Course list">
+                <CourseList listCourses={listCourses} />
+              </BodySectionWithMarginBottom>
+              ) : (
+              <BodySectionWithMarginBottom title="Log in to continue">
+                <Login />
+              </BodySectionWithMarginBottom>)}
+            <BodySection title="News from the School">
+              <p>Lorem Ipsum</p>
+            </BodySection>
+          </div>
+          <div className="App-footer">
+            <Footer />
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+  logOut: PropTypes.func,
+};
+
+App.defaultProps = {
+  isLoggedIn: false,
+  logOut: () => {},
+};
---
+++ 0x04-React_inline_styling/task_0/dashboard/src/Login/Login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import WithLogging from '../HOC/WithLogging';
+import './Login.css';
+
+export default function Login() {
+  return (
+		<React.Fragment>
+			<p>Login to access the full dashboard</p>
+			<form className="Form-body">
+				<label htmlFor="email">
+					Email:
+					<input type="email" id="email" name="email" />
+				</label>
+				<label htmlFor="password">
+					Password:
+					<input type="password" id="password" name="password" />
+				</label>
+				<input type="submit" value="OK" />
+			</form>
+		</React.Fragment>
+  );
+}
