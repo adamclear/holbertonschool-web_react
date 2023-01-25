c7cd45f32580ab5dea635f9426eb4537cd823339
adamclear <3589@holbertonschool.com>
2023-01-25 18:35:27 +0000

    updated for task 2

---
+++ 0x01-React_Immutable/2-nested.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// eslint-disable-next-line no-unused-vars
+
+import Immutable from 'immutable';
+
+export default function accessImmutableObject(obj, array) {
+	return Immutable.fromJS(obj).getIn(array);
+}
