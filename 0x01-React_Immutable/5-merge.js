845489bef415fb68ce1701dfc551813826a97b10
adamclear <3589@holbertonschool.com>
2023-01-25 18:52:24 +0000

    updated for task 5

---
+++ 0x01-React_Immutable/5-merge.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+import Immutable from 'immutable';
+
+export function concatElements(page1, page2) {
+  return Immutable.List((page1).concat(page2));
+}
+
+export function mergeElements(page1, page2) {
+  return Immutable.Map(page1).merge(page2);
+}
