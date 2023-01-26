87d391d5fedc81df487bbb3cdfd3a885c1b4a28f
adamclear <3589@holbertonschool.com>
2023-01-26 16:00:26 +0000

    updated

--- 0x01-React_Immutable/8-seq.js
+++ 0x01-React_Immutable/8-seq.js
@@ -2,9 +2,7 @@
 import Immutable from 'immutable';
 
 export default function printBestStudents(grades) {
-  const aboveSeventy = Immutable.Seq(grades).filter(
-    (grade) => grade.score >= 70
-  );
+  const aboveSeventy = Immutable.Seq(grades).filter((grade) => grade.score >= 70);
   console.log(aboveSeventy.map((student) => ({
     score: student.score,
     firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
