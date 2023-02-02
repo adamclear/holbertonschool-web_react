261ebe09b45df3c5f7a42ba7f1319016956ff368
adamclear <3589@holbertonschool.com>
2023-02-02 16:17:17 +0000

    added directory and files for task 0

---
+++ 0x02-react_redux_action_creator_normalizr/task_0/dashboard/src/schema/notifications.js
@@ -0,0 +1,6 @@
+import notifs from '../../notifications.json';
+
+export default function getAllNotificationsByUser(userId) {
+	return notifs.filter((notif) => notif.author.id === userId)
+		.map((notif) => notif.context);
+}
