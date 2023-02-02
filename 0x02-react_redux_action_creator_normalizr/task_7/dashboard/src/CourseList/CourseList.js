cf76d2c5efa65202945b0c23b32187348d99c364
adamclear <3589@holbertonschool.com>
2023-02-02 18:09:52 +0000

    updated for task 7

---
+++ 0x02-react_redux_action_creator_normalizr/task_7/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import CourseListRow from './CourseListRow';
+import CourseShape from './CourseShape';
+import PropTypes from 'prop-types';
+import { css, StyleSheet } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  table: {
+		margin: '5rem 0 0 2.5rem',
+		border: '1px solid lightgrey',
+		width: '90%',
+		textAlign: 'left'
+	},
+	
+	thead: {
+		fontWeight: 'bold',
+		':nth-child(1n) > :first-child': {
+			textAlign: 'center'
+		}
+	}
+});
+
+CourseList.propTypes = {
+	listCourses: PropTypes.arrayOf(CourseShape),
+};
+
+CourseList.defaultProps = {
+	listCourses: [],
+};
+
+export default function CourseList({ listCourses }) {
+	return (
+		<table id="CourseList" className={`table ${css(styles.table)}`}>
+			<thead className={`thead ${css(styles.thead)}`}>
+				<CourseListRow
+					textFirstCell='Available courses'
+					isHeader={true} />
+				<CourseListRow
+					textFirstCell='Course name'
+					isHeader={true}
+					textSecondCell='Credit' />
+			</thead>
+			<tbody>
+				{listCourses.length ? (listCourses.map((course) => (
+					<CourseListRow key={course.id}
+						textFirstCell={course.name}
+						textSecondCell={course.credit} />))) : (
+					<CourseListRow textFirstCell='No course available yet' />)}
+			</tbody>
+		</table>
+	);
+}
---
+++ 0x02-react_redux_action_creator_normalizr/task_7/dashboard/src/actions/notificationActionCreators.js
@@ -0,0 +1,23 @@
+import { MARK_AS_READ, SET_TYPE_FILTER } from "./notificationActionTypes";
+import { bindActionCreators } from "redux";
+
+export function markAsRead(index) {
+	return {
+		type: MARK_AS_READ,
+		index
+	};
+}
+
+export function setNotificationFilter(filter) {
+	return {
+		type: SET_TYPE_FILTER,
+		filter
+	};
+}
+
+export const notificationActions = {
+	markAsRead,
+	setNotificationFilter
+}
+
+export const boundNotificationActions = (dispatch) => bindActionCreators(notificationActions, dispatch)
